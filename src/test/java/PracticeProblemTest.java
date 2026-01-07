4e4315a9c64808a869739e55c6d50a5827506f62
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2026-01-07 13:28:18 -0500

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class PracticeProblemTest {
 
    @Test
-   public void testOutput()
+   public void q1Test()
    {
      PrintStream originalOut = System.out;
      ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -15,35 +15,66 @@ public class PracticeProblemTest {
      PracticeProblem.q1();
 
      // assertion
-     assertEquals("There once was a man from St. Ives.\n", bos.toString());
+     assertEquals(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n\n* * * * *\n*       *\n*       *\n*       *\n* * * * *\n\n* * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n", bos.toString());
 
      // undo the binding in System
      System.setOut(originalOut);
    }
 
    @Test
-   public void testInputandOutput()
+   public void q2Test()
    {
-      String data = "Users Input";
-      System.setIn(new ByteArrayInputStream(data.getBytes()));
-      
-      PrintStream originalOut = System.out;
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(bos));
+     PrintStream originalOut = System.out;
+     ByteArrayOutputStream bos = new ByteArrayOutputStream();
+     System.setOut(new PrintStream(bos));
 
-      // action
-      PracticeProblem.q1();
+     // action
+     PracticeProblem.q2();
 
-      // assertion
-      assertEquals("There once was a man from St. Ives.\n", bos.toString());
+     // assertion
+     assertEquals("          '__'\n          (oo)\n  /========//\n / || XX ||\n*  ||----||\n   VV    VV\n   ''    ''\n", bos.toString());
 
-      // undo the binding in System
-      System.setOut(originalOut);
+     // undo the binding in System
+     System.setOut(originalOut);
    }
 
    @Test
-   public void testQ3()
+   public void q3Test()
    {
-     
+     PrintStream originalOut = System.out;
+     ByteArrayOutputStream bos = new ByteArrayOutputStream();
+     System.setOut(new PrintStream(bos));
+
+     // action
+     PracticeProblem.q3();
+
+     // assertion
+     assertEquals("Fortnite is awful\n", bos.toString());
+
+     // undo the binding in System
+     System.setOut(originalOut);
    }
+
+   
+
+   @Test
+   public void q4Test()
+   {
+     PrintStream originalOut = System.out;
+     ByteArrayOutputStream bos = new ByteArrayOutputStream();
+     System.setOut(new PrintStream(bos));
+
+     // action
+     PracticeProblem.q4();
+
+     // assertion
+     assertEquals("Don't @ me\n", bos.toString());
+
+     // undo the binding in System
+     System.setOut(originalOut);
+   }
+
+   
+
+   
 }
