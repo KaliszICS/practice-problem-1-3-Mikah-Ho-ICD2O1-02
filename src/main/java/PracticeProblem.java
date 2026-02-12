46d9dbaa7391d52c3b3b67fa05cda44b488d6fd1
Mikah-Ho-ICD2O1-02 <mikah.ho@student.tdsb.on.ca>
2026-02-12 16:30:02 +0000

    is that a cat or a cow

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,10 @@
+/**
+* File Name: Lesson 1.3 - Strings
+* Author: Mikah Ho
+* Date Created: Feb 12, 2026
+* Date Last Modified: Feb 12, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +16,48 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		
+		System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+		System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+		System.out.println("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *");
+
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+
+		String uglyCow;
+		uglyCow = "          '__'\n          (oo)\n  /========//\n / || XX ||\n*  ||----||\n   VV    VV\n   ''    ''";
+
+		System.out.println(uglyCow);
+
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+
+		String fortnite;
+		String isAwful;
+
+		fortnite = "Fortnite ";
+		isAwful = "is awful";
+
+		System.out.print(fortnite);
+		System.out.println(isAwful);
+
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+
+		String string1;
+		String string2;
+
+		string1 = "Don't ";
+		string2 = "@ me";
+
+		System.out.println(string1 + string2);
+
 	}
 
 
